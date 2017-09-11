503047acf395bcd3491c7fcd23d5adc4c820865c
agoalofalife <agoalofalife@gmail.com>
2017-09-11 22:07:05 +0300

    add command enc

--- commands/add.go
+++ commands/add.go
@@ -13,11 +13,14 @@ var cmdAdd = &cobra.Command{
 	Use:     "add",
 	Short:   "Add or create files(or anything else)",
 	Long:    ``,
-	Example: "add config $GOPATH/src/... (where is your project)",
+	Example: `
+	 - add config $GOPATH/src/... (where is your project)
+	 - add env $GOPATH/src/... (where is your project)
+	`,
 	Args:    cobra.MinimumNArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
 		var nameConfFile = "app.json"
-
+		var envName = ".env"
 		typeAction := args[0]
 
 		switch typeAction {
@@ -26,6 +29,14 @@ var cmdAdd = &cobra.Command{
 				_, filename, _, _ := runtime.Caller(0)
 				utils.Copy(args[1]+nameConfFile, path.Dir(filename)+"/../stubs/config/"+nameConfFile)
 
+				fmt.Fprintf(color, "%sCreate default configuration file : %s %s\n", "\x1b[32m", "\x1b[0m", args[1]+nameConfFile)
+			} else {
+				fmt.Fprintf(color, "%sDirectory %s is not exist.%s\n", "\x1b[31m", args[1], "\x1b[0m")
+			}
+		case "env":
+			if utils.DirExist(args[1]) {
+				_, filename, _, _ := runtime.Caller(0)
+				utils.Copy(args[1]+nameConfFile, path.Dir(filename)+"/../stubs/config/"+envName)
 				fmt.Fprintf(color, "%sCreate default configuration file : %s %s\n", "\x1b[32m", "\x1b[0m", args[1]+nameConfFile)
 			} else {
 				fmt.Fprintf(color, "%sDirectory %s is not exist.%s\n", "\x1b[31m", args[1], "\x1b[0m")

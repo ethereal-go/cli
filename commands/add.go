d7e5fd5a15e8197d7a612d798992593457d8e3c0
agoalofalife <agoalofalife@gmail.com>
2017-09-12 11:10:28 +0300

    fix commands

--- commands/add.go
+++ commands/add.go
@@ -19,23 +19,23 @@ var cmdAdd = &cobra.Command{
 	`,
 	Args: cobra.MinimumNArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
-		var nameConfFile = "app.json"
-		var envName = ".env"
+		var (
+			nameConfFile = "app.json"
+			envName      = ".env"
+		)
 		typeAction, NewPath := args[0], args[1]
+		_, filename, _, _ := runtime.Caller(0)
 
 		switch typeAction {
 		case "config":
 			if utils.DirExist(NewPath) {
-				_, filename, _, _ := runtime.Caller(0)
 				utils.Copy(NewPath+nameConfFile, path.Dir(filename)+"/../stubs/config/"+nameConfFile)
-
 				fmt.Fprintf(color, "%sCreate default configuration file : %s %s\n", "\x1b[32m", "\x1b[0m", NewPath+nameConfFile)
 			} else {
 				fmt.Fprintf(color, "%sDirectory %s is not exist.%s\n", "\x1b[31m", NewPath, "\x1b[0m")
 			}
 		case "env":
 			if utils.DirExist(NewPath) {
-				_, filename, _, _ := runtime.Caller(0)
 				utils.Copy(NewPath+nameConfFile, path.Dir(filename)+"/../stubs/config/"+envName)
 				fmt.Fprintf(color, "%sCreate default configuration file : %s %s\n", "\x1b[32m", "\x1b[0m", NewPath+nameConfFile)
 			} else {
